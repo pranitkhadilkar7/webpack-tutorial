5e03150b57b449f0df3cdf0a194a5437129b6317
Pranit Khadilkar <pranit@emtech.com>
2023-10-24 14:26:26 +0530

    added sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,12 @@ module.exports = {
                 use: [
                     'style-loader', 'css-loader'
                 ]
+            },
+            {
+                test: /\.scss/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
             }
         ]
     },
