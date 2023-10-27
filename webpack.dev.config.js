2a8663519cb02634ed9c208b06a6bca579fa0991
Pranit Khadilkar <pranit@emtech.com>
2023-10-27 10:47:43 +0530

    code splitting in dev mode

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,9 +3,12 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        'hello-world': './src/hello-world.js',
+        'logo': './src/logo.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         publicPath: '',
     },
@@ -76,7 +79,16 @@ module.exports = {
             ]
         }),
         new HtmlWebpackPlugin({
-            title: 'Webpack Tutorial'
-        })
+            filename: 'hello-world.html',
+            chunks: ['hello-world'],
+            title: 'Hello World',
+            minify: false
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'logo.html',
+            chunks: ['logo'],
+            title: 'Logo',
+            minify: false
+        }),
     ],
 }
